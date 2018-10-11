991c70cfacff55595aeb1c1c942e60d4dc7a9abb
chema969 <chema969@hotmail.com>
2018-10-11 13:31:55 +0200

    fuck

--- Practica2/auxiliarFunctions.cpp
+++ Practica2/auxiliarFunctions.cpp
@@ -18,6 +18,7 @@ cv::Mat createBoxFilter(int r){
           ptr[x]=(float)1/(r*r);    
        }
      }
+   return aux;
     
 }
 
@@ -25,7 +26,8 @@ void filt(cv::Mat& in,cv::Mat& filter, cv::Mat& out){
   for(int y=0;y<in.rows;y++){
        float *ptr=out.ptr<float>(y);
        for(int x=0;x<in.cols;x++){
-           if((y>(filter.rows/2))&&(x>(filter.cols/2))&&((y-in.rows)>filter.rows/2)&&((x-in.cols)>filter.cols/2)){
+           //std::cout<<"Y="<<y<<", filter rows/2 ="<<filter.rows/2<<", x="<<x<<", filter cols/2="<< filter.cols/2<<std::endl;
+           if((y>(filter.rows/2))&&(x>(filter.cols/2))&&((in.rows-y)>filter.rows/2)&&((in.cols-x)>filter.cols/2)){
                float result=0;
                int ry=0; int rx=0;
                for(int j=y-filter.rows/2;j<=y+filter.rows/2;j++){
@@ -33,6 +35,7 @@ void filt(cv::Mat& in,cv::Mat& filter, cv::Mat& out){
                      float *ptrconv=in.ptr<float>(j);           
                      for(int k=x-filter.rows/2;k<=x+filter.rows/2;k++){
                           result+=(ptrfilter[rx]*ptrconv[k]);
+                          std::c
                           rx++;
                           }
                      ry++;
@@ -44,12 +47,6 @@ void filt(cv::Mat& in,cv::Mat& filter, cv::Mat& out){
       }
    }
 
-     for(int y=0;y<in.rows;y++){
-       float *ptr=out.ptr<float>(y);
-       for(int x=0;x<in.cols;x++){
-          std::cout<<ptr[x]<<std::endl;
-       }
-     }
 }
 
 void convolve(const cv::Mat& in, const cv::Mat& filter, cv::Mat& out){
