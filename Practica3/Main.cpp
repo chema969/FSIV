4cffe2f9cc960215568bed3d689003667acd8b69
chema969 <chema969@hotmail.com>
2018-10-31 13:29:13 +0100

    ojala funcione xd

--- Practica3/Main.cpp
+++ Practica3/Main.cpp
@@ -35,17 +35,32 @@ int main(int argc,char **argv){
    while(video.read(frame)){
        aux l;
        cv::Mat corners;
-       cv::Size xy(size/2,size/2);
+       cv::Size xy(rows,cols);
         cv::Mat frameButInGrey;
         cv::cvtColor(frame, frameButInGrey, cv::COLOR_BGR2GRAY);
-        
-        cv::findChessboardCorners(frame,xy,corners);
-        //cv::cornerSubPix(frameButInGrey,corners,xy, cv::Size(-1,-1), cv::TermCriteria());
+        std::vector<cv::Point3f> objectPoints;
+        for(int y=0; y<cols; ++y) {
+           for(int x=0; x<rows; ++x)
+              objectPoints.push_back(cv::Point3f(x*size,y*size,0));
+        }               
+        bool patternfound=cv::findChessboardCorners(frame,xy,corners);
+        cv::cornerSubPix(frameButInGrey,corners,cv::Size(5,5), cv::Size(-1,-1), cv::TermCriteria());
         readYML(intrinsics,l);
         cv::Mat rvec,tvec;
-       /* cv::imshow("Live", frame);
+    //    cv::drawChessboardCorners(frame, xy, cv::Mat(corners), patternfound);
+    
+      std::vector<cv::Point2f> output;
+       cv::solvePnP(cv::Mat(objectPoints),cv::Mat(corners),l.cameraMatrix1,l.distortionCoefficient1,rvec,tvec);
+     cv::projectPoints(cv::Mat(objectPoints),cv::Mat(rvec), cv::Mat(tvec), l.cameraMatrix1,l.distortionCoefficient1, output);
+     
+
+      for(int y=0;y<output.size()-1;y++){
+      cv::line(frame,output[y], output[y+1], const Scalar& color)
+      }
+    
+       cv::imshow("Live", output);
         if (cv::waitKey(5) >= 0)
-            break;*/
+            break;
   }
 
 
